c991b3638e421d3c28de56e22af13d457f15fab7
Anshumaan Singh <xnshumxxn@gmail.com>
2026-06-13 01:27:58 +0530

    first

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Timer from "@/components/timer";
-
-export default function Home() {
-  return (
-    <main>
-      <Timer />
-    </main>
-  );
-}
--- components/timer.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function timer() {
-  return (
-    <div>
-      <h2>25:00</h2>
-    </div>
-  );
-}
